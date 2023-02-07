d53c37a63a2f4bd5380f8412fdc9a6e5f54cc1b7
sarang2551 <74551805+sarang2551@users.noreply.github.com>
2023-02-07 14:44:27 +0800

    Basic LoginPage setup

---
+++ app/src/main/java/com/example/loginpage/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.loginpage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText Name;
+    private EditText Password;
+    private TextView Info;
+    private Button Login;
+    private ImageView Profile;
+    private int counter = 5;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Name = (EditText) findViewById(R.id.ETUsername);
+        Password = (EditText) findViewById(R.id.ETPassword);
+        Info = (TextView) findViewById(R.id.TVAttemptsLeft);
+        Login = (Button) findViewById(R.id.LoginButton);
+        Profile = (ImageView) findViewById(R.id.imageView);
+
+        Info.setText("Number of attempts remaining: 5");
+
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                validate(Name.getText().toString(), Password.getText().toString());
+            }
+        });
+
+    }
+
+    private void validate(String userUsername, String userPassword) {
+        if ((userUsername == "Admin") && (userPassword == "1234")) {
+            Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+            startActivity(intent);
+        } else {
+            counter--;
+
+            Info.setText("Number of attempts remaining: " + String.valueOf(counter));
+
+            if (counter == 0) {
+                Login.setEnabled(false);
+            }
+        }
+
+    }
+}
