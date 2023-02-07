fb80463453990fbdac1c4c287f618c685bb0519d
sarang2551 <74551805+sarang2551@users.noreply.github.com>
2023-02-07 16:33:42 +0800

    With the help of the gods, I managed to install dependencies and instantiate the client class.

--- app/src/main/java/com/example/loginpage/MainActivity.java
+++ app/src/main/java/com/example/loginpage/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.loginpage;
 
 import androidx.appcompat.app.AppCompatActivity;
+import io.getstream.chat.android.client.ChatClient;
+import io.getstream.chat.android.client.api.ChatClientConfig;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -11,6 +13,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
 
     private EditText Name;
     private EditText Password;
@@ -23,22 +26,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String api_key = "r9ckvmqw8u97";
+        ChatClient client = new ChatClient.Builder(api_key,getApplicationContext()).build();
 
+    }
+    private void init(){
         Name = (EditText) findViewById(R.id.ETUsername);
         Password = (EditText) findViewById(R.id.ETPassword);
         Info = (TextView) findViewById(R.id.TVAttemptsLeft);
         Login = (Button) findViewById(R.id.LoginButton);
         Profile = (ImageView) findViewById(R.id.imageView);
-
         Info.setText("Number of attempts remaining: 5");
 
         Login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                validate(Name.getText().toString(), Password.getText().toString());
+                validate(Name.getText().toString(),Password.getText().toString());
             }
         });
-
     }
 
     private void validate(String userUsername, String userPassword) {
@@ -56,4 +61,52 @@ public class MainActivity extends AppCompatActivity {
         }
 
     }
+    public EditText getName() {
+        return Name;
+    }
+
+    public void setName(EditText name) {
+        Name = name;
+    }
+
+    public EditText getPassword() {
+        return Password;
+    }
+
+    public void setPassword(EditText password) {
+        Password = password;
+    }
+
+    public TextView getInfo() {
+        return Info;
+    }
+
+    public void setInfo(TextView info) {
+        Info = info;
+    }
+
+    public Button getLogin() {
+        return Login;
+    }
+
+    public void setLogin(Button login) {
+        Login = login;
+    }
+
+    public ImageView getProfile() {
+        return Profile;
+    }
+
+    public void setProfile(ImageView profile) {
+        Profile = profile;
+    }
+
+    public int getCounter() {
+        return counter;
+    }
+
+    public void setCounter(int counter) {
+        this.counter = counter;
+    }
+
 }
