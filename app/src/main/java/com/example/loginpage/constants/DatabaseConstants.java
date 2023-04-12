bf0ba9a5cd2cb7e6de9aad05fd22447a50858a83
sarang2551 <74551805+sarang2551@users.noreply.github.com>
2023-04-12 15:35:54 +0800

    Database constants added

---
+++ app/src/main/java/com/example/loginpage/constants/DatabaseConstants.java
@@ -0,0 +1,16 @@
+package com.example.loginpage.constants;
+
+/**
+ * @author saran
+ * @date 12/4/2023
+ */
+
+public class DatabaseConstants {
+    public static String CHANNELS = "Channels";
+    public static String MESSAGES = "messages";
+    public static String REPLIES = "Replies";
+    public static String EXTRA_DATA = "extraData";
+    public static String USERS = "users";
+    public static String DATABASE_NAME = "d-project-8fd93-default-rtdb";
+    public static String DATABASE_REGION = "asia-southeast1";
+}
--- app/src/main/java/com/example/loginpage/utility/Database.java
+++ app/src/main/java/com/example/loginpage/utility/Database.java
@@ -3,6 +3,8 @@ package com.example.loginpage.utility;
 
 import android.util.Log;
 
+import com.example.loginpage.constants.DatabaseConstants;
+import com.example.loginpage.constants.ExtraData;
 import com.google.android.gms.tasks.Task;
 import com.google.android.gms.tasks.TaskCompletionSource;
 import com.google.firebase.database.DataSnapshot;
@@ -20,19 +22,10 @@ import io.getstream.chat.android.client.models.Message;
  */
 
 public class Database {
-   private final String databaseName = "d-project-8fd93-default-rtdb";
-   private final String databaseRegion = "asia-southeast1";
    private FirebaseDatabase database;
    private DatabaseReference baseReference;
    private static Database sDatabase;
    private DatabaseReference channelReference;
-   private final String CHANNELS = "Channels";
-   private final String MESSAGES = "messages";
-   private final String REPLIES = "Replies";
-   private final String EXTRA_DATA = "extraData";
-   private final String USERS = "users";
-   private final String VOTE_COUNT = "vote_count";
-   private final String REPLY_COUNT = "RC";
 
    public static Database getInstance() {
       if (sDatabase == null) {
@@ -45,46 +38,24 @@ public class Database {
       connect();
    }
 
-   public Task<Boolean> checkChannel(String channelId) {
-      TaskCompletionSource<Boolean> taskCompletionSource = new TaskCompletionSource<>();
-
-      DatabaseReference channelRef = channelReference.child(channelId);
-
-      channelRef.addListenerForSingleValueEvent(new ValueEventListener() {
-         @Override
-         public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-            // If the dataSnapshot exists, then the channel exists
-            boolean channelExists = dataSnapshot.exists();
-            taskCompletionSource.setResult(channelExists);
-         }
-
-         @Override
-         public void onCancelled(@NonNull DatabaseError databaseError) {
-            // Handle the error case here
-            taskCompletionSource.setException(databaseError.toException());
-         }
-      });
-
-      return taskCompletionSource.getTask();
-   }
 
    public void storeDetails(String userId, String username, String selectedRole) {
-      baseReference.child(USERS).child(userId).child("username").setValue(username);
-      baseReference.child(USERS).child(userId).child("role").setValue(selectedRole);
+      baseReference.child(DatabaseConstants.USERS).child(userId).child("username").setValue(username);
+      baseReference.child(DatabaseConstants.USERS).child(userId).child("role").setValue(selectedRole);
    }
 
    public Task<Void> sendMessage(String channelId, Message message) {
       // writing task
-      return channelReference.child(channelId).child(MESSAGES).child(message.getId()).setValue(message);
+      return channelReference.child(channelId).child(DatabaseConstants.MESSAGES).child(message.getId()).setValue(message);
    }
 
    public Task<DataSnapshot> getRole(String uid) {
-      return baseReference.child("users").child(uid).child("role").get();
+      return baseReference.child(DatabaseConstants.USERS).child(uid).child("role").get();
    }
 
    public Task<DataSnapshot> getQuestionText(String channelId, String messageId) {
       // reading task
-      return channelReference.child(channelId).child(MESSAGES).child(messageId).child("text").get();
+      return channelReference.child(channelId).child(DatabaseConstants.MESSAGES).child(messageId).child("text").get();
    }
    public Task<Void>tickPressed(String channelId, String messageId,String user){
       return getExtraDataForMessage(channelId, messageId).child(user).setValue("true");
@@ -99,69 +70,69 @@ public class Database {
 
    }
    public Task<DataSnapshot>getReplyTickPressed(String channelId, String replyId,String user){
-      return channelReference.child(channelId).child(REPLIES).child(replyId).child(EXTRA_DATA).child(user).get();
+      return channelReference.child(channelId).child(DatabaseConstants.REPLIES).child(replyId).child(DatabaseConstants.EXTRA_DATA).child(user).get();
 
    }
    public Task<Void>ReplyTickPressed(String channelId, String replyId,String user){
-      return channelReference.child(channelId).child(REPLIES).child(replyId).child(EXTRA_DATA).child(user).setValue("true");
+      return channelReference.child(channelId).child(DatabaseConstants.REPLIES).child(replyId).child(DatabaseConstants.EXTRA_DATA).child(user).setValue("true");
 
    }
    public Task<Void>ReplyTickRemoved(String channelId, String replyId,String user){
-      return channelReference.child(channelId).child(REPLIES).child(replyId).child(EXTRA_DATA).child(user).setValue("false");
+      return channelReference.child(channelId).child(DatabaseConstants.REPLIES).child(replyId).child(DatabaseConstants.EXTRA_DATA).child(user).setValue("false");
 
    }
 
 
    public Task<Void> upVoteMessage(String channelId, String messageId, int votes) {
-      return getExtraDataForMessage(channelId, messageId).child(VOTE_COUNT).setValue(votes);
+      return getExtraDataForMessage(channelId, messageId).child(ExtraData.VOTE_COUNT).setValue(votes);
    }
 
    public Task<Void> upVoteReply(String channelId, String replyId, int votes) {
-      return channelReference.child(channelId).child(REPLIES).child(replyId).child(EXTRA_DATA).child(VOTE_COUNT).setValue(votes);
+      return channelReference.child(channelId).child(DatabaseConstants.REPLIES).child(replyId).child(DatabaseConstants.EXTRA_DATA).child(ExtraData.VOTE_COUNT).setValue(votes);
    }
 
    public Task<DataSnapshot> getVoteCount(String channelId, String messageId) {
-      return getExtraDataForMessage(channelId, messageId).child(VOTE_COUNT).get();
+      return getExtraDataForMessage(channelId, messageId).child(ExtraData.VOTE_COUNT).get();
    }
 
    public Task<DataSnapshot> getReplyCountForMessage(String channelId, String messageId) {
-      return getExtraDataForMessage(channelId, messageId).child(REPLY_COUNT).get();
+      return getExtraDataForMessage(channelId, messageId).child(ExtraData.REPLY_COUNT).get();
    }
 
    public Task<Void> updateReplyCountForMessage(String channelId, String messageId, int newCount) {
-      return getExtraDataForMessage(channelId, messageId).child(REPLY_COUNT).setValue(newCount);
+      return getExtraDataForMessage(channelId, messageId).child(ExtraData.REPLY_COUNT).setValue(newCount);
    }
 
    public DatabaseReference getExtraDataForMessage(String channelId, String messageId) {
-      return channelReference.child(channelId).child(MESSAGES).child(messageId).child(EXTRA_DATA);
+      return channelReference.child(channelId).child(DatabaseConstants.MESSAGES).child(messageId).child(DatabaseConstants.EXTRA_DATA);
    }
    public Task<DataSnapshot> getExtraDataForMessage_diff(String channelId, String messageId) {
-      return channelReference.child(channelId).child(MESSAGES).child(messageId).child(EXTRA_DATA).get();
+      return channelReference.child(channelId).child(DatabaseConstants.MESSAGES).child(messageId).child(DatabaseConstants.EXTRA_DATA).get();
    }
    public Task<DataSnapshot> getExtraDataForReplies_diff(String channelId, String replyId) {
-      return channelReference.child(channelId).child(REPLIES).child(replyId).child(EXTRA_DATA).get();
+      return channelReference.child(channelId).child(DatabaseConstants.REPLIES).child(replyId).child(DatabaseConstants.EXTRA_DATA).get();
    }
    public DatabaseReference getExtraDataForReply(String channelId, String replyId) {
 
-      return channelReference.child(channelId).child(REPLIES).child(replyId).child(EXTRA_DATA);
+      return channelReference.child(channelId).child(DatabaseConstants.REPLIES).child(replyId).child(DatabaseConstants.EXTRA_DATA);
    }
 
    public Task<DataSnapshot> getReplyUpVoteCount(String channelId, String replyId) {
-      return channelReference.child(channelId).child(REPLIES).child(replyId).child(EXTRA_DATA).child(VOTE_COUNT).get();
+      return channelReference.child(channelId).child(DatabaseConstants.REPLIES).child(replyId).child(DatabaseConstants.EXTRA_DATA).child(ExtraData.VOTE_COUNT).get();
    }
 
    public Task<Void> sendReply(String channelId_messageId, Message reply) {
-      return channelReference.child(channelId_messageId).child(REPLIES).child(reply.getId()).setValue(reply);
+      return channelReference.child(channelId_messageId).child(DatabaseConstants.REPLIES).child(reply.getId()).setValue(reply);
    }
 
    public Task<Void> deleteReply(String channelId_messageId, String replyId) {
-      return channelReference.child(channelId_messageId).child(REPLIES).child(replyId).removeValue();
+      return channelReference.child(channelId_messageId).child(DatabaseConstants.REPLIES).child(replyId).removeValue();
    }
    public DatabaseReference getRepliesReference(String uid){
-      return baseReference.child(USERS).child(uid).child(REPLIES);
+      return baseReference.child(DatabaseConstants.USERS).child(uid).child(DatabaseConstants.REPLIES);
    }
    public Task<Void> sendReplyToHistory(String uid, Message reply){
-      return baseReference.child(USERS).child(uid).child(REPLIES).child(reply.getId()).setValue(reply);
+      return baseReference.child(DatabaseConstants.USERS).child(uid).child(DatabaseConstants.REPLIES).child(reply.getId()).setValue(reply);
    }
 
    public Task<Void> deleteMessage(String channelId, String messageId) {
@@ -170,18 +141,18 @@ public class Database {
       channelReference.child(correspondingReplyChannelId).removeValue().addOnCompleteListener(data->{
          Log.i("Database","Corresponding reply channel: " + correspondingReplyChannelId + " for message: " + messageId + " is deleted");
       });
-      return channelReference.child(channelId).child(MESSAGES).child(messageId).removeValue();
+      return channelReference.child(channelId).child(DatabaseConstants.MESSAGES).child(messageId).removeValue();
    }
    public DatabaseReference getMessagesReference(String channelId){
-      return channelReference.child(channelId).child(MESSAGES);
+      return channelReference.child(channelId).child(DatabaseConstants.MESSAGES);
    }
 
    public void connect() {
       try {
-         String referenceId = "https://d-project-8fd93-default-rtdb.asia-southeast1.firebasedatabase.app/";
+         String referenceId = "https://" + DatabaseConstants.DATABASE_NAME + "." + DatabaseConstants.DATABASE_REGION + ".firebasedatabase.app/";
          database = FirebaseDatabase.getInstance(referenceId);
          baseReference = database.getReference();
-         channelReference = baseReference.child(CHANNELS);
+         channelReference = baseReference.child(DatabaseConstants.CHANNELS);
       } catch (Exception e){
          Log.e("Database","Error connecting to database: " + e);
 
